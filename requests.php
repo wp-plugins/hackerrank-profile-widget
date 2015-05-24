bc480ee1eb47bc1d1cc81196d6eeef2866eaf43a
hacdias <hacdias@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-05-24 19:07:38 +0000

    v1.0.3
    git-svn-id: https://plugins.svn.wordpress.org/hackerrank-profile-widget/trunk@1166754 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- requests.php
+++ requests.php
@@ -5,22 +5,44 @@
  * @package HackerRank Profile Widget
  * @author Henrique Dias <hacdias@gmail.com>
  * @author Luís Soares <lsoares@gmail.com>
- * @version 1.0.0
+ * @version 1.0.3
  */
 
-require_once 'requests.functions.php';
+function hackerrank_get_url_variables() {
+	$pathInfo = '';
 
-$url = hackerRankGetUrlVariables();
+	if ( isset( $_SERVER['REDIRECT_URL'] ) ) {
+		$pathInfo = $_SERVER['REDIRECT_URL'];
+	}
 
-if (count($url) < 1) {
-    exit('Few arguments.');
+	if ( isset( $_SERVER['PATH_INFO'] ) ) {
+		$pathInfo = $_SERVER['PATH_INFO'];
+	}
+
+	return preg_split( '|/|', $pathInfo, - 1, PREG_SPLIT_NO_EMPTY );
 }
 
-$user = $url[0];
-$method = isset($url[1]) ? $url[1] : null;
-$queryString = isset($_GET) ? http_build_query($_GET) : null;
+function hackerrank_get_content( $user, $method = null, $query = null ) {
+	$user .= '/';
+	if ( isset( $method ) ) {
+		$method .= '?';
+	}
+	$url = "https://www.hackerrank.com/rest/hackers/{$user}{$method}{$query}";
 
-header('Access-Control-Allow-Origin: *');
-header('Content-Type: application/json');
+	return file_get_contents( $url );
+}
 
-echo hackerRankGetHackerRankContent($user, $method, $queryString);
+$url = hackerrank_get_url_variables();
+
+if ( count( $url ) < 1 ) {
+	exit( 'Few arguments.' );
+}
+
+$user        = $url[0];
+$method      = isset( $url[1] ) ? $url[1] : null;
+$queryString = isset( $_GET ) ? http_build_query( $_GET ) : null;
+
+header( 'Access-Control-Allow-Origin: *' );
+header( 'Content-Type: application/json' );
+
+echo hackerrank_get_content( $user, $method, $queryString );
